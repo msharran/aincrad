0ce5a972d4185002b629ab58f26059453fcc812f
Sharran <sharran.murali@gmail.com>
2026-04-07 07:45:23 +0530

    final configs

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,8 @@
 return {
-    "ydkulks/cursor-dark.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme("cursor-dark")
-      require("cursor-dark").setup({
-        -- For theme
-        -- style = "dark-midnight",
-        -- For a transparent background
-        -- transparent = true,
-      })
-    end,
-  }
+	"rose-pine/neovim",
+	name = "rose-pine",
+	config = function()
+        -- vim.o.background = "light" -- or "light" for light mode
+		vim.cmd("colorscheme rose-pine")
+	end
+}
